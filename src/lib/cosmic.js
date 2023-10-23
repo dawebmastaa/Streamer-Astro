8bc09823d9d8071ef7117923f780d8cffff24d9a
Mike Alberts <mikealberts@gmail.com>
2023-10-23 04:39:13 -0400

    Working good with Tailwind, Cosmic CMS, dynamic blog, navigation and pages.

---
+++ src/lib/cosmic.js
@@ -0,0 +1,60 @@
+import { createBucketClient } from '@cosmicjs/sdk'
+
+const BUCKET_SLUG = import.meta.env.PUBLIC_COSMIC_BUCKET_SLUG
+const READ_KEY = import.meta.env.PUBLIC_COSMIC_READ_KEY
+
+const cosmic = createBucketClient({
+  bucketSlug: BUCKET_SLUG,
+  readKey: READ_KEY
+})
+
+export async function getAllPosts() {
+  const data = await cosmic.objects
+    .find({
+      type: 'posts'
+    })
+    .props('title,slug,metadata,created_at,excerpt')
+    .sort('-created_at')
+    .depth(2)
+  return data.objects
+}
+
+export async function getFeaturedPost() {
+  const data = await cosmic.objects
+    .findOne({
+      type: 'featured-post',
+      slug: 'set-featured-post'
+    })
+    .props('metadata')
+    .depth(2)
+  return data.object.metadata.post
+}
+
+export async function getAllPages() {
+  const data2 = await cosmic.objects
+    .find({
+      type: 'pages'
+    })
+    .props('title,slug,metadata,created_at')
+    .depth(2)
+  return data2.objects
+}
+
+export async function getFeaturedPage() {
+  const data = await cosmic.objects
+    .findOne({
+      type: 'page',
+      slug: 'set-featured-post'
+    })
+    .props('metadata')
+    .depth(2)
+  return data.object.metadata.post
+}
+
+export async function getConfig() {
+  const data = await cosmic.objects
+    .findOne({ type: 'config', slug: 'config' })
+    .props('metadata')
+    .depth(1)
+  return data.object
+}
