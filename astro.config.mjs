bbb2fc3396eb36449f008dec35ccf2ba8f6a8165
Mike  Alberts <mikealberts@gmail.com>
2026-07-17 18:55:24 -0400

    Rebuild to change doimain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   outDir: './docs',
+  site: 'https://streamer.ruletheweb.us/',
   integrations: [tailwind(),  sitemap()]
 });
